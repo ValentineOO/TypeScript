30e430be3d58951481b82d1abe761b19685e7af2
ValentineOO <varlomxg@gmail.com>
2023-09-27 11:31:40 +0100

    doing well with TS

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,25 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let sales = 123456789;
+let course = "Typescript";
+let is_published = true;
+let level;
+level = 1;
+level = "high";
+function render(document) {
+    console.log(document);
+}
+let numbers = [];
+numbers.forEach((n) => n.toPrecision);
+let user = [1, "Adams"];
+user[1];
+user.push(1);
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+;
+let mySize = Size.Medium;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
 let sales = 123_456_789;
 let course = "Typescript";
-let is_published = true; 
+let is_published = true;
+
+let level;
+level = 1;
+level = "high";
+
+function render(document: any) {
+  console.log(document);
+}
+
+//arrays
+
+let numbers: number[] = [];
+// numbers [0] = 1;
+// numbers [1] = "1";
+
+numbers.forEach((n) => n.toPrecision);
+
+// Turples
+
+let user: [number, string] = [1, "Adams" ]
+user[1]
+user.push(1)
+
+// enums
+
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+
+// PascalCase
+enum Size { Small = 1, Medium, Large };
+let mySize: Size = Size.Medium;
+console.log( mySize);
+
